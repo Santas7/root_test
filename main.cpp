a6318ae2b48db2ad4c377c6df8445935568b813a
Andrey <86359412+Santas7@users.noreply.github.com>
2023-03-08 13:53:57 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
+#include <string>
+#include <limits>
+
+std::string to_binary_string(int n)
+{
+    if (n == std::numeric_limits<int>::min())
+        return "-1" + std::string(std::numeric_limits<int>::digits, '0');
+    std::string buffer;
+    buffer.reserve(std::numeric_limits<int>::digits + 1); 
+    bool negative = (n < 0);
+    if (negative) n = -n;
+    do
+    {
+        buffer += char('0' + n % 2);
+        n = n / 2;
+    } while (n > 0);
+    if (negative) buffer += '-';
+    return std::string(buffer.crbegin(), buffer.crend());
+}
 
 int get_double_value(int x){
   return x*x;
